b16ddd738304058b178eb0d142d63ea3348e0837
Yasser <yasser.vazquez85@gmail.com>
2025-03-18 16:24:30 -0400

    Seccion About del Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import homeCSS from "./Home.module.css";
 import heroImg from "../../assets/home_1-slider_01_royma.png";
+// import aboutImg from "../../assets/home_01-1.png";
+import aboutImg from "../../assets/home_1-slider_01_royma.png";
 
 function Home() {
   return (
@@ -64,6 +66,40 @@ function Home() {
               <p>Creative Website for You</p>
             </div>
             <i className="ri-arrow-right-s-line"></i>
+          </div>
+        </div>
+      </section>
+
+      {/* About Section */}
+      <section className={homeCSS.about_wrapper}>
+        <div className={homeCSS.about_header}>
+          <img src={aboutImg} alt="about-image" />
+        </div>
+        <div className={homeCSS.about_content}>
+          <small className="title">about Me</small>
+          <h2>
+            Need Creative Design ? <br /> I Can Help You
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
+            veniam ea tempore numquam dolorum placeat, alias fugit nulla
+            inventore mollitia cumque unde nam, obcaecati totam in. Corporis
+            vitae delectus id vero fugit?
+          </p>
+          <h4>You Can Follow Me Also Here</h4>
+          <div className={homeCSS.social_accounts}>
+            <div className={homeCSS.social_accounts_card}>
+              <i className="ri-behance-line"></i>
+              <h2>Behance</h2>
+            </div>
+            <div className={homeCSS.social_accounts_card}>
+              <i className="ri-dribbble-line"></i>
+              <h2>Dribbble</h2>
+            </div>
+            <div className={homeCSS.social_accounts_card}>
+              <i className="ri-instagram-line"></i>
+              <h2>Instagram</h2>
+            </div>
           </div>
         </div>
       </section>
